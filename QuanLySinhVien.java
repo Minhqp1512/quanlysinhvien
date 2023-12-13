42741dff2083e1a794b8e09f00f2bbbde8290c33
Minh Phạm Quang <82825240+Minhqp1512@users.noreply.github.com>
2023-12-13 10:41:45 +0700

    Add files via upload

---
+++ QuanLySinhVien.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlysinhvien;
+
+/**
+ *
+ * @author Admin
+ */
+public class QuanLySinhVien {
+
+    public static void main(String[] args) {
+        SinhVien sinhVien = new SinhVien();
+
+        System.out.println("Nhap thong tin sinh vien:");
+        sinhVien.nhapThongTin();
+
+        sinhVien.hienThiThongTin();
+
+        if (sinhVien.laSinhVienGioi()) {
+            System.out.println("Sinh vien thuoc loai gioi.");
+        } else {
+            System.out.println("Sinh vien khong thuoc loai gioi.");
+        }
+    }
+}
